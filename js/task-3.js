25fe3755fe3bfebdcc4e93307877c8c9ac0473c5
Taras1000 <semkivtaras80@gmail.com>
2023-12-03 21:21:43 +0000

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,15 @@
 'use strict';
 function checkForSpam(message) {
-    const result = message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale") ? true : false;
-    return result;
+
+    const toLowerCaseMessage = message.toLowerCase();
+    
+    return toLowerCaseMessage.includes("spam") || toLowerCaseMessage.includes("sale");
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
